ddf289fddb229a127e62cc887ec0a38eac60c2a5
cuprum-acid <jony.zh@yandex.ru>
2021-02-13 14:32:47 +0300

    Add First screen

---
+++ app/src/main/java/in/blogspot/kmvignesh/calculator/MenuScreen.kt
@@ -0,0 +1,11 @@
+package `in`.blogspot.kmvignesh.calculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_screen)
+    }
+}
